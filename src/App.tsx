8edb9bf956b1802a6af691612f1702c16efa3f2c
Raymond Zhuang <z5592481@ad.unsw.edu.au>
2026-02-12 21:07:12 +1100

    Used custom hooks to integrate side effects

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,18 @@ type Story = {
   points: number;
 };
 
+const useStorageState = (key: string, initialState: string) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue] as const
+};
+
 const App = () => {
   const stories = [
     {
@@ -29,13 +41,15 @@ const App = () => {
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState(
-    localStorage.getItem("search") || 'React'
-  );
+  const[searchTerm, setSearchTerm] = useStorageState("search", "React");
 
-  React.useEffect(() => {
-    localStorage.setItem("search", searchTerm);
-  }, [searchTerm]);
+  // const [searchTerm, setSearchTerm] = React.useState(
+  //   localStorage.getItem("search") || 'React'
+  // );
+
+  // React.useEffect(() => {
+  //   localStorage.setItem("search", searchTerm);
+  // }, [searchTerm]);
 
   const handleSearch = (
     event: React.ChangeEvent<HTMLInputElement>
